e4729d0e101ed899915798a0d0497a8641cde4a0
EC2 Default User <ec2-user@ip-172-31-30-240.ap-northeast-1.compute.internal>
2022-09-23 12:49:53 +0000

    バリデーションにて開始日に今日以前の日付を選べないこと、開始日＝終了日にできないことを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
   validates :title, presence: true
   validates :start_at, presence: true
-  validate :date_before_start
-  def date_before_start
-    return if start_at.blank?
-    errors.add(:start_at, "は今日以降のものを選択してください") if start_at < Date.today
-  end
   validate :not_reverse_start_finish
   def not_reverse_start_finish
-    errors.add(:start_at, "は終了日以前の日付にしてください") if start_at >= finish
+    errors.add(:start_at, "は終了日以前の日付にしてください") if start_at > finish
   end  
   validates :finish, presence: true
   validates :onememo, length: {maximum: 40}
